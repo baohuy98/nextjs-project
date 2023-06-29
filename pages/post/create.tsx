a5cfe0a6b6dd2bced7946ddaf4966d8466434b77
baohuydev <lenambaohuy1998@gmail.com>
2023-06-29 10:54:27 +0700

    learning

---
+++ pages/post/create.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+export interface CreatePostPageProps {
+}
+
+export default function CreatePostPage(props: CreatePostPageProps) {
+    return (
+        <div>
+            <h1>create post page</h1>
+        </div>
+    );
+}
